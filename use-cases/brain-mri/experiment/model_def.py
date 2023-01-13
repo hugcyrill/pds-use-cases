107022ae5b7e9b93d0c57a24e87bcb625606ea6c
Cyrill Hug <32030867+hugcyrill@users.noreply.github.com>
2023-01-13 13:06:40 -0600

    Update model_def.py

--- use-cases/brain-mri/experiment/model_def.py
+++ use-cases/brain-mri/experiment/model_def.py
@@ -27,9 +27,9 @@ class MRIUnetTrial(PyTorchTrial):
             data_dir = self.data_config["data_dir"]
             full_dir = os.path.join(full_dir, download_dir.strip("/"), data_dir.strip("/"))
 
-            des = self.download_data(self.data_config, download_dir)
+            des = self.download_data(self.data_config, full_dir)
 
-            print("Download Directory = " + download_dir)
+            print("Download Directory = " + full_dir)
 
             self.train_dataset, self.val_dataset = data.get_train_val_datasets(
                 download_dir,
